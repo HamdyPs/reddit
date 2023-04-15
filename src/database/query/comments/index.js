1c2017128eeb44f8b820f478cb92b9f94a97d698
HamdyPs <redwanhmdyrdwan@gmail.com>
2023-04-15 04:17:57 +0300

    add vote to each post and each vote relate to 1 post Relate #15 closes #15

--- src/database/query/comments/index.js
+++ src/database/query/comments/index.js
@@ -12,6 +12,17 @@ const createCommentQuery = (description, userId, postId) => {
 
   return connection.query(sql)
 };
+const addVoteQuery = ( userId, postId) => {
+  console.log(userId, postId);
+  const sql = {
+    text: `INSERT INTO votes
+     (user_id, post_id)
+     VALUES ($1 , $2)`,
+    values: [userId, postId]
+  }
+
+  return connection.query(sql)
+};
 
 
-module.exports = { createCommentQuery }
+module.exports = { createCommentQuery, addVoteQuery }
