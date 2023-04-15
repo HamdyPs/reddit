dfa55a7ffcc9dc02e6b53fbec9147564b4b4dfdd
HamdyPs <redwanhmdyrdwan@gmail.com>
2023-04-15 16:12:20 +0300

    create apis router and countroller Relate #19 closes #19

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 const userRouter = require('./user');
 const postsRouter = require('./posts');
 const commentsRouter = require('./comments');
+const apiRouter = require('./apis');
 // const { clientError, serverError } = require('../controller/error/error');
 
 const router = require('express').Router();
@@ -8,6 +9,7 @@ const router = require('express').Router();
 router.use('/users', userRouter)
 router.use('/posts', postsRouter)
 router.use('/comments', commentsRouter)
+router.use('/apis', apiRouter)
 // router.use(clientError);
 // router.use(serverError);
 
