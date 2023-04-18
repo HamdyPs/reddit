fb3c3c8580449727306acd75c16e2ecfdb278e88
HamdyPs <redwanhmdyrdwan@gmail.com>
2023-04-18 16:07:25 +0300

    get user data backend and database Relate #63 closes #63

--- src/router/user/index.js
+++ src/router/user/index.js
@@ -1,10 +1,12 @@
 const userRouter = require('express').Router();
-const { signUp, signin, getSignUpPage,getProfilePage} = require('../../controller/users')
+const { signUp, signin, getSignUpPage,getProfilePage, getUserData} = require('../../controller/users')
+const auth = require('../../helper/auth');
 
 userRouter.post('/signup', signUp);
 userRouter.post('/signin', signin);
 userRouter.get('/resgister', getSignUpPage)
 userRouter.get('/profile', getProfilePage)
+userRouter.get('/setting',auth, getUserData)
 
 
 module.exports = userRouter
