3dc7198861cd33ec82f8dce96402916cc8f99e91
HamdyPs <redwanhmdyrdwan@gmail.com>
2023-04-18 17:51:32 +0300

    create update user data functionality fronend Relate #69 closes #69

--- public/js/setting.js
+++ public/js/setting.js
@@ -1,3 +1,4 @@
+
 const username = document.querySelector('.username');
 const email = document.querySelector('.email');
 const photo = document.querySelector('.photo');
@@ -5,7 +6,9 @@ const date = document.querySelector('.date');
 const country = document.querySelector('.country');
 const phone = document.querySelector('.phone');
 const address = document.querySelector('.address');
-axios.get('/api/users/sitting').then(response=>{
+const uncheckedIcon = document.querySelectorAll('.fa-user-pen');
+const checkedIcon = document.querySelectorAll('.fa-user-check');
+axios.get('/api/users/sitting').then(response => {
   username.value = response.data[0].username;
   email.value = response.data[0].email;
   photo.value = response.data[0].photo;
@@ -13,5 +16,42 @@ axios.get('/api/users/sitting').then(response=>{
   country.value = response.data[0].country;
   phone.value = response.data[0].phone;
   address.value = response.data[0].address;
+})
+
+uncheckedIcon.forEach(icon => {
+  icon.addEventListener('click', () => {
+    username.removeAttribute('disabled')
+    email.removeAttribute('disabled')
+    photo.removeAttribute('disabled')
+    date.removeAttribute('disabled')
+    country.removeAttribute('disabled')
+    phone.removeAttribute('disabled')
+    address.removeAttribute('disabled')
+  })
+})
+checkedIcon.forEach(icon => {
+  icon.addEventListener('click', () => {
+    username.setAttribute('disabled', true)
+    email.setAttribute('disabled', true)
+    photo.setAttribute('disabled', true)
+    date.setAttribute('disabled', true)
+    country.setAttribute('disabled', true)
+    phone.setAttribute('disabled', true)
+    address.setAttribute('disabled', true)
+    const data = {
+      username: username.value,
+      email: email.value,
+      photo: photo.value,
+      date: date.value,
+      country: country.value,
+      phone: phone.value,
+      address: address.value
+    }
+    axios.put('/api/users/update', {
+      body: data
+    })
+
+
+  })
 })
 
