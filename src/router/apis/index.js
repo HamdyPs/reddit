7a5aa9b873c196c61199a4bf90d270e8e4f49c10
HamdyPs <redwanhmdyrdwan@gmail.com>
2023-04-26 18:39:49 +0300

    fix project

--- src/router/apis/index.js
+++
@@ -1,7 +0,0 @@
-const apiRouter = require('express').Router();
-const {getNarutoApi,getFreeGamesApi,getRandomAnimeApi} = require('../../controller/apis')
-apiRouter.get('/naruto',getNarutoApi)
-apiRouter.get('/games',getFreeGamesApi)
-apiRouter.get('/animes',getRandomAnimeApi)
-
-module.exports = apiRouter
